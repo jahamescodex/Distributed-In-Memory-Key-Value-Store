bcbd09bf8b5e65c464fc0c723b76ee5d17b2fb6b
James <jfcodex@gmail.com>
2026-05-31 09:37:48 -0500

    finished handleClient function

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
-//go:build ignore
-
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	contactBook := NewContactBookMap()
+	jobsPipe := make(chan net.Conn, 100)
+
+	for w := 1; w <= 10; w++ { // starting 10 workers
+		go process(jobsPipe, contactBook)
+	}
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("Error listening:", err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting conn:", err)
+			continue
+		}
+		jobsPipe <- conn
+	}
 }
--- server.go
+++ server.go
@@ -1,3 +1,69 @@
-//go:build ignore
-
 package main
+
+import (
+	"log"
+	"net"
+	"strings"
+)
+
+type cmd struct {
+	opCode string
+	args   []string
+}
+
+func process(jobs <-chan net.Conn, contactBook *contactBookMap) {
+	for conn := range jobs {
+		handleClient(conn, contactBook)
+	}
+}
+
+func handleClient(connection net.Conn, contactBook *contactBookMap) {
+	defer connection.Close()
+	var buff [1024]byte
+	writePos := 0
+
+	for {
+		n, err := connection.Read(buff[writePos : writePos+1])
+		if err != nil {
+			return
+		}
+		if n == 0 {
+			continue
+		}
+		currentByte := buff[writePos]
+
+		if currentByte == '\n' {
+			line := buff[:writePos]
+			if len(line) > 0 && line[len(line)-1] == '\r' {
+				line = line[:len(line)-1]
+			}
+
+			commandLine := strings.Fields(string(line))
+
+			if len(commandLine) == 0 {
+				continue
+			}
+
+			parser := &cmd{
+				opCode: commandLine[0],
+				args:   commandLine[1:],
+			}
+
+			processRequest(parser, contactBook)
+
+			writePos = 0
+			continue
+		}
+		writePos++
+
+		if writePos >= len(buff) {
+			log.Printf("Oversized payload from %s - closing wire", connection.RemoteAddr())
+			connection.Write([]byte("ERR Command too long\n"))
+			return
+		}
+	}
+}
+
+func processRequest(input *cmd, contactBook *contactBookMap) {
+
+}
