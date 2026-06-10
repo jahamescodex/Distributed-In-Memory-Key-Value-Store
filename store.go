420555a11a994566525f0864e97deca0ebb9160a
James <jfcodex@gmail.com>
2026-06-09 22:01:26 -0500

    Refactored to prevent memory leaks

--- store.go
+++ store.go
@@ -1,50 +1,68 @@
 package main
 
-import (
-	"sync"
-)
+import "sync"
 
 type contactBookMap struct {
 	contactBook map[string]Record
 	lock        sync.RWMutex
-	ID          uint
+	HWM         int
+	counterOPS  int
 }
 
 type Record struct {
-	UID uint
-	val []byte
+	data []byte
+	ID   int
 }
 
-func (c *contactBookMap) Set(key string, val []byte) {
+func (c *contactBookMap) Set(key string, val []byte) { //recieve a struct which has a pointer
 	c.lock.Lock()
 	defer c.lock.Unlock()
-	c.ID++
-	reserve := make([]byte, len(val))
-	copy(reserve, val)
+
+	c.counterOPS++ // 1 - Key : Value
+
+	copyVal := make([]byte, len(val))
+	copy(copyVal, val) //dst, src
 	c.contactBook[key] = Record{
-		UID: c.ID,
-		val: reserve,
-	}
-}
+		data: copyVal,
+		ID:   c.counterOPS,
+	} // increased the length of the map
 
-func (c *contactBookMap) Delete(key string) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
-	delete(c.contactBook, key)
+	if len(c.contactBook) > c.HWM {
+		c.HWM = len(c.contactBook)
+	}
 }
 
 func (c *contactBookMap) Get(key string) ([]byte, bool) {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
+
 	val, ok := c.contactBook[key]
 	if !ok {
 		return nil, false
 	}
-	return val.val, true
+	return val.data, true
+}
+
+func (c *contactBookMap) Delete(key string) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	currentHWM := c.HWM
+	delete(c.contactBook, key)
+	if len(c.contactBook) < (currentHWM/4) && len(c.contactBook) > 8 {
+		copyMap := make(map[string]Record, len(c.contactBook))
+		for k, v := range c.contactBook {
+			copyMap[k] = v
+		}
+		c.contactBook = copyMap
+		c.HWM = len(c.contactBook)
+	}
 }
 
 func NewContactBookMap() *contactBookMap {
 	return &contactBookMap{
 		contactBook: make(map[string]Record),
+		HWM:         0,
+		counterOPS:  0,
 	}
 }
